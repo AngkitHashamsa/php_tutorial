eadcc322eb41297375b3cb598a978077e013a582
angkit <angkithashamsa21@gmail.com>
2022-06-14 18:40:57 +0530

    dissable sing and loggin when login and remove logout when not lognin

--- components/_nav.php
+++ components/_nav.php
@@ -1,4 +1,11 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<?php
+$loggedIn = false;
+if (isset($_SESSION['loggin']) == true) {
+    global $loggedIn;
+    $loggedIn = true;
+}
+;
+echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
     <a class="navbar-brand" href="/phpAngkit/welcome.php">Anime</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,21 +15,25 @@
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/phpAngkit/welcome.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/phpAngkit/login.php">Login</a>
+        </li>';
+if ($loggedIn == true) {
+    echo '<li class="nav-item">
+          <a class="nav-link" href="/phpAngkit/logout.php">Log out</a>
+        </li>';
+} else {
+    echo '<li class="nav-item">
+        <a class="nav-link" href="/phpAngkit/login.php">Login</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/phpAngkit/signup.php">Sign Up</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/phpAngkit/logout.php">Log out</a>
-        </li>
-      </ul>
+      ';
+}
+echo '</ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </div>
-</nav>
+</nav>';
